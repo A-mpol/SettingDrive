8889ffd3f15e73bbc193248978866e19f7a6f7a0
Настя Полякова <114810379+A-mpol@users.noreply.github.com>
2024-07-04 14:57:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include "drive-setup.hpp"
+#include <iostream>
 
 int main() {
     Drive dorna("/dev/tty.usbserial-AH011Z8K");
     dorna.TurnOn();
-    dorna.MoveToPosition(30000);
+    dorna.MoveToPosition(0);
+    while (!dorna.InPosition()) {
+        int pos = dorna.GetDrivePosition();
+        std::cout << pos << '\n';
+    }
+    dorna.SwitchOff();
     return 0;
 }
